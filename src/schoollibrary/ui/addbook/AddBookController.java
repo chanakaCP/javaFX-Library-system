41bb6844e8b0af328fee5098e0e2e95a059f9c99
chanakaCP <praveenchanaka415@gmail.com>
2019-10-09 18:55:39 +0530

    Modify add book

--- src/schoollibrary/ui/addbook/AddBookController.java
+++ src/schoollibrary/ui/addbook/AddBookController.java
@@ -1,11 +1,17 @@
 
 package schoollibrary.ui.addbook;
 
+import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXDatePicker;
 import com.jfoenix.controls.JFXTextField;
 import java.net.URL;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.time.LocalDate;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.beans.value.ObservableValue;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -24,6 +30,10 @@ public class AddBookController implements Initializable {
     private JFXTextField b_id;
     @FXML
     private JFXTextField b_name;
+     @FXML
+    private JFXComboBox<String> catSelector;
+    @FXML
+    private JFXTextField category;
     @FXML
     private JFXTextField author;
     @FXML
@@ -39,10 +49,33 @@ public class AddBookController implements Initializable {
    
     DatabaseHandler databaseHandler;
     MainController mainController;
- 
+    
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         databaseHandler = DatabaseHandler.getInstance();
+        category.setDisable(true);
+//        catSelector.getItems().add("Add New Category");
+//
+//        String query = "SELECT DISTINCT category FROM BOOK";
+//        ResultSet result = databaseHandler.execQuery(query);
+//        try {
+//            while (result.next()) {
+//                catSelector.getItems().add(result.getString("category"));
+//            }
+//        } catch (SQLException ex) {
+//            Logger.getLogger(AddBookController.class.getName()).log(Level.SEVERE, null, ex);
+//        } 
+        catSelector.getItems().add("Add New Category");
+        initComboBox();   
+
+        catSelector.valueProperty().addListener((ObservableValue<? extends String> observable, String oldValue, String newValue) -> {
+            if(newValue.equals("Add New Category")){
+                category.setDisable(false);
+            }else{
+                category.setDisable(true);
+            }
+        });
+
     }    
 
     @FXML
@@ -55,7 +88,7 @@ public class AddBookController implements Initializable {
         String bookPrice = price.getText();
         String bookPage = pages.getText();
         LocalDate bookRecieveDate = r_date.getValue();
-        
+        String bookCategory;
         try{
             float bookPricefloat = Float.parseFloat(bookPrice);
         }catch (NumberFormatException e ) {
@@ -76,13 +109,28 @@ public class AddBookController implements Initializable {
             return;
         } 
         
-        String query = "INSERT INTO BOOK (B_ID,BName,author,publisher,price,pages,receivedDate,description) "
-        + "VALUES ( '" + bookID + "','" + bookName + "','" + bookAuthor + "','" + bookPublisher + "'," +bookPrice + "," +bookPage + ",'" + bookRecieveDate + "','" + bookDescription + "')";
-        
+        if(category.isDisable()){
+            bookCategory = catSelector.getValue();
+        }else{
+            bookCategory = category.getText();
+        }
+              
+        String query = "INSERT INTO BOOK (B_ID,BName,category,author,publisher,price,pages,receivedDate,description) "
+                        + "VALUES ( '" + bookID + "','" 
+                                       + bookName + "','" 
+                                       + bookCategory + "','" 
+                                       + bookAuthor + "','" 
+                                       + bookPublisher + "'," 
+                                       + bookPrice + "," 
+                                       + bookPage + ",'" 
+                                       + bookRecieveDate + "','" 
+                                       + bookDescription + "')";
+        System.out.println(query);
         if(databaseHandler.execAction(query)){
             AlertMaker.informatinAlert("Success","Insert Book Successfully"); 
             b_id.setText("");
             b_name.setText("");
+            category.setText("");
             author.setText("");
             publisher.setText("");
             price.setText("");
@@ -90,8 +138,10 @@ public class AddBookController implements Initializable {
             pages.setText("");
             r_date.setValue(null);
             mainController.refreshGraph();
+           
+            initComboBox();
         }else{
-            AlertMaker.errorAlert("Can`t save","This book is alredy exsist");
+            AlertMaker.errorAlert("Can`t save","Please fill all the fields correctly");
         }   
     }
 
@@ -105,5 +155,20 @@ public class AddBookController implements Initializable {
     public void getController(MainController mainController){  
         this.mainController = mainController;
     }
+
+    private void initComboBox() {
+       
+        String query = "SELECT DISTINCT category FROM BOOK";
+        ResultSet result = databaseHandler.execQuery(query);
+        try {
+            while (result.next()) {
+                catSelector.getItems().add(result.getString("category"));
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(AddBookController.class.getName()).log(Level.SEVERE, null, ex);
+        }   
+    }
+
+   
     
 }
